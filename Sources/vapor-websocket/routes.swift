27f8034502f1eb6bef6db0b14b07b3272fbfce28
kangddong <rkdehddud96@naver.com>
2025-04-06 01:33:20 +0900

    add websocket method

--- Sources/vapor-websocket/routes.swift
+++ Sources/vapor-websocket/routes.swift
@@ -1,10 +1,23 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-app.get { req async in
+    app.webSocket("echo") { req, ws in
+        print("🔌 WebSocket connected")
+        
+        ws.onText { ws, text in
+            print("📩 Received: \(text)")
+            ws.send("Echo: \(text)")
+        }
+        
+        ws.onClose.whenComplete { _ in
+            print("❌ WebSocket disconnected")
+        }
+    }
+    
+    app.get { req async in
         "It works!"
     }
-
+    
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
